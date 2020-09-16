e042eebe70f4a3d217e56bc1997415f240555281
Kirill Larionov <kirill.larionov@visma.com>
2020-09-16 16:18:00 +0200

    Initial commit

---
+++ API/Controllers/GetController.cs
@@ -0,0 +1,53 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Flurl;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("/")]
+    public class GetController : ControllerBase
+    {
+        private HttpClient _httpClient;
+        private Options _options;
+
+        public GetController(HttpClient httpClient, Options options)
+        {
+            _httpClient = httpClient;
+            _options = options;
+        }
+
+        private async Task<BlobContainerClient> GetCloudBlobContainer(string containerName)
+        {
+            BlobServiceClient serviceClient = new BlobServiceClient(_options.StorageConnectionString);
+            BlobContainerClient containerClient = serviceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+            return containerClient;
+        }
+
+        [Route("/get/")]
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> Get()
+        {
+            BlobContainerClient containerClient = await GetCloudBlobContainer(_options.FullImageContainerName);
+            List<string> results = new List<string>();
+            await foreach (BlobItem blobItem in containerClient.GetBlobsAsync())
+            {
+                results.Add(
+                    Flurl.Url.Combine(
+                        containerClient.Uri.AbsoluteUri,
+                        blobItem.Name
+                    )
+                );
+            }
+            Console.Out.WriteLine("Got Images");
+            return Ok(results);
+        }
+    }
+}
---
+++ API/PostController.cs
@@ -0,0 +1,45 @@
+﻿using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
+using Flurl;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Api.Controllers
+{
+
+    public class PostController : ControllerBase
+    {
+        private HttpClient _httpClient;
+        private Options _options;
+
+        public PostController(HttpClient httpClient, Options options)
+        {
+            _httpClient = httpClient;
+            _options = options;
+        }
+
+        private async Task<BlobContainerClient> GetCloudBlobContainer(string containerName)
+        {
+            BlobServiceClient serviceClient = new BlobServiceClient(_options.StorageConnectionString);
+            BlobContainerClient containerClient = serviceClient.GetBlobContainerClient(containerName);
+            await containerClient.CreateIfNotExistsAsync();
+            return containerClient;
+        }
+
+        [Route("/post/")]
+        [HttpPost]
+        public async Task<ActionResult> Post()
+        {
+            Stream image = Request.Body;
+            BlobContainerClient containerClient = await GetCloudBlobContainer(_options.FullImageContainerName);
+            string blobName = Guid.NewGuid().ToString().ToLower().Replace("-", String.Empty);
+            BlobClient blobClient = containerClient.GetBlobClient(blobName);
+            await blobClient.UploadAsync(image);
+            return Created(blobClient.Uri, null);
+        }
+    }
+}
